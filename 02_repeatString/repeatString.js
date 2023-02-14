d841b97f934c0d440eca05adb17ebc6029a8d119
Steven Aiello <stevenaiello634@yahoo.com>
2023-02-14 11:57:17 -0700

    1-3 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result ='';
+    if(num<0){
+        return "ERROR"
+    }
+    while(num!=0){
 
+        num = num -1;
+        result =result.concat(string);
+    }
+    if(num ==0){
+        return result;
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array= string.split("");
+    let result= array.reverse();
+    result= result.join('');
+    return result;
 };
 
 // Do not edit below this line
