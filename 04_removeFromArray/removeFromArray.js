370baaae56f968b7dc09f244f3e6e311f9c3674e
Steven Aiello <stevenaiello634@yahoo.com>
2023-02-16 18:16:54 -0700

    skipped one worked on the next one

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...arg) {
+    let removeMe=[...arg];
+    let oG=[arr];
+    
+    do{   //recursivly going through ...args until are expended
+        while(oG.indexOf(removeMe[0])!=-1){   // this will return -1 if second arg is in first arg
+            oG.forEach((el)=>{   //loop through oG and remove
+                if(el===removeMe[0]){
+                    oG=oG.splice(el,1)
+                }
+            }
+        )}
+
+        removeMe.shift()
+        console.log("removeMe= "+removeMe);
+        console.log("oG= "+oG);
+    }
+    while(removeMe.length!=0);
+    return oG;
+
 
 };
-
+removeFromArray([1,2,3,4,5,6,7,8,9,10],1,0,1,0,"fat",20,9,-1)
 // Do not edit below this line
-module.exports = removeFromArray;
+//module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
+    let firstArg =0;
+    let secArg = 0;
+    let sum =0;
 
+    if(arg1===NaN||arg2===NaN){
+        console.log("error")
+        return"ERROR"
+    }if(arg1<0||arg2<0){
+        console.log("error")
+        return"ERROR"
+    }if(arg1===arg2){
+        return arg1
+    }if(arg1<arg2){
+        firstArg=firstArg+arg1;
+        secArg=secArg+arg2;
+    }else{
+        firstArg=firstArg+arg2;
+        secArg=secArg+arg1;
+    }
+
+    for(let i =firstArg; i>=secArg;i++){
+        sum=sum+i;
+        console.log(sum)
+    }
+    return sum;
 };
-
+//console.log(sumAll(2,5))
+//console.log(sumAll(0,-1))
+//console.log(sumAll(2,[1,2]))
+//console.log(sumAll(2,"5"))
 // Do not edit below this line
-module.exports = sumAll;
+//module.exports = sumAll;
