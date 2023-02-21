186556da51c75d6191161e59fe73ed56c71b00b4
Steven Aiello <stevenaiello634@yahoo.com>
2023-02-20 20:56:56 -0700

    finished leapYears and removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,25 +1,20 @@
 const removeFromArray = function(arr, ...arg) {
-    let removeMe=[...arg];
-    let oG=[arr];
-    
-    do{   //recursivly going through ...args until are expended
-        while(oG.indexOf(removeMe[0])!=-1){   // this will return -1 if second arg is in first arg
-            oG.forEach((el)=>{   //loop through oG and remove
-                if(el===removeMe[0]){
-                    oG=oG.splice(el,1)
-                }
+    let newArr=[];
+
+    for(let i =0; i<arr.length; i++){
+        let shouldRemove =false;
+        for(let j =0; j< arg.length ;j++){
+            if(arr[i]===arg[j]){
+                shouldRemove=true;
+                break
             }
-        )}
-
-        removeMe.shift()
-        console.log("removeMe= "+removeMe);
-        console.log("oG= "+oG);
-    }
-    while(removeMe.length!=0);
-    return oG;
-
-
+        }
+        if(!shouldRemove){
+            newArr.push(arr[i]);
+        };
+    };
+    return newArr;
 };
-removeFromArray([1,2,3,4,5,6,7,8,9,10],1,0,1,0,"fat",20,9,-1)
+
 // Do not edit below this line
-//module.exports = removeFromArray;
+module.exports = removeFromArray;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leapYear= false
+    //leap years are divisable by 4
+    if(year%4==0){
+        leapYear=true;
+    };
+    if(year%100==0){
+        leapYear=false;
+    };
+    if(year%400==0){
+        leapYear=true;
+    };
+    return leapYear
+    //not divisable by 100(1800,1900)
+        //unless they are also divisable by 400
 };
 
 // Do not edit below this line
