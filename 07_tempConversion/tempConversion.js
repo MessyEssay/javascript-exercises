3fed61c28baf4be9697f298760002b4011aeb91e
Steven Aiello <stevenaiello634@yahoo.com>
2023-02-22 20:13:59 -0700

    C to F ok F to C not yet

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((fahrenheit-32)*.556)*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius*1.8)+32)*10)/10
 };
+//Celsius to Fahrenheit, multiply by 1.8 (or 9/5) and add 32.
 
 // Do not edit below this line
+//console.log('To Celsius '+convertToCelsius(32) )
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
